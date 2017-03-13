3a31be7b2e330265588afc4e5fd2c40354417f2a
wintercherry <wuxiaohui@foreveross.com>
2017-03-13 10:58:37 +0800

    第一次上传整个项目

---
+++ www/showanimation2048.js
@@ -0,0 +1,5 @@
+/**
+ * Created by liuyubobobo on 14-4-11.
+ * my site: http://www.liuyubobobo.com
+ */
+
---
+++ www/support2048.js
@@ -0,0 +1,72 @@
+/**
+ * Created by liuyubobobo on 14-4-11.
+ * my site: http://www.liuyubobobo.com
+ */
+
+
+function getPositonTop( i , j ){
+    return 20 + i*120;
+}
+
+function getPositonLeft( i , j ){
+    return 20 + j*120;
+}
+
+function getNumberBackgroundColor(number){
+ switch( number ){
+			  case 2:return "#eee4da";break;
+			  case 4:return "#ede0c8";break;
+			  case 8:return "#f2b179";break;
+			  case 16:return "#f59563";break;
+			  case 32:return "#f67c5f";break;
+			  case 64:return "#f65e3b";break;
+			  case 128:return "#edcf72";break;
+			  case 256:return "#edcc61";break;
+			  case 512:return "#9c0";break;
+			  case 1024:return "#33b5e5";break;
+			  case 2048:return "#09c";break;
+			  case 4096:return "#a6c";break;
+			  case 8192:return "#93c";break;
+			  default:return "black";
+			  
+  }
+
+	// switch(number){
+	// 	case 2:
+	// 		return "#fd9400";
+	// 		break;
+	// 	case 4:
+	// 		return "#fd0c00";
+	// 		break;			
+	// 	case 8:
+	// 		return "#0024ff";
+	// 		break;			
+	// 	case 16:
+	// 		return "#f600ff";
+	// 		break;			
+	// 	case 32:
+	// 		return "#00fff0";
+	// 		break;			
+	// 	case 64:
+	// 		return "#6a005f";
+	// 		break;			
+	// 	case 128:
+	// 		return "#36ff00";
+	// 		break;	
+	// 	case 256:
+	// 		return "#ab6400";
+	// 		break;			
+	// 	case 512:
+	// 		return "#00ab5d";
+	// 		break;			
+	// 	default:		
+	// 		return "#ff5400";
+	// }
+}
+
+function getNumberColor( number ){
+    if( number <= 4 )
+        return "#776e65";
+
+    return "white";
+}
