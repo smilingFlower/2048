b4bb27e554cf34e4b8ff9ff0c61f7796f37f752a
wintercherry <wuxiaohui@foreveross.com>
2017-03-13 11:03:22 +0800

    移动main2048.js到www文件夹

---
+++ www/main2048.js
@@ -0,0 +1,128 @@
+/**
+ * Created by liuyubobobo on 14-4-11.
+ * my site: http://www.liuyubobobo.com
+ */
+
+var board = new Array(); 
+var score = 0; //游戏分数
+
+$(function(){
+
+	newGame();
+
+});
+
+
+function newGame(){
+	//初始化棋盘格
+	init();
+
+	//随机生成两个数
+	generateNumber();
+	generateNumber();
+
+}
+
+
+function init(){
+
+	//初始化每个格子的位置
+	for (var i = 0; i < 4; i++) { //i 行
+		for (var j = 0; j < 4; j++) { //j 列
+
+			var gridCell = $("#grid-cell-" + i + "-" + j);
+
+			gridCell.css({
+				left:getPositonTop(i,j) + "px",
+				top:getPositonLeft(i,j) + "px"
+			});
+			
+		}
+	}
+
+	for (var i = 0; i < 4; i++) {
+		board[i] = new Array();
+    for( var j = 0 ; j < 4 ; j ++ ){
+        board[i][j] = 0;
+    }		
+	}
+
+	updateBoardView();
+
+}
+
+
+function  updateBoardView(){
+
+	$(".number-cell").remove();
+
+	for (var i = 0; i < 4; i++) {
+		for (var j = 0; j < 4; j++) {
+
+			$(".game-container").append("<div class='number-cell' id='number-cell-"+ i + "-" + j +"'></div>");
+
+			var numberCellID = $("#number-cell-"+ i + "-" + j);
+
+
+			if ( board[i][j] === 0 ) {
+				numberCellID.css({
+					"display" : "none"
+				})
+			}else{
+				numberCellID.css({
+					"display" : "block",
+					"left" : calculateSpacing(i) + "px",
+					"top" : calculateSpacing(j) + "px",
+					"backgroundColor":getNumberBackgroundColor( board[i][j] ),
+					"color":getNumberColor( board[i][j] ),
+				})
+			}//if结束
+
+		}
+	}//for结束
+
+}
+
+function generateNumber(){
+
+	//如果没有位置就不再生成
+	if (isGenerateNumber()) {
+		return
+	}
+
+	//位置
+	var line = parseInt(Math.floor(Math.random * 4));
+	var column = parseInt(Math.floor(Math.random * 4));
+
+	if(isGenerateNumber(line,column)){
+		board[i][j] = getInitNumber();
+	}
+
+	updateBoardView()
+}
+
+
+function getInitNumber(){
+	var num;
+	return parseInt(Math.random() * 2) == 2 ? num = 2 : num = 4;
+	return num;
+}
+
+function isGenerateNumber(i,j){
+	var isGenerateNumber;
+
+	if (typeof i !== "undefined") {
+		return board[i][j] == 0 ? isGenerateNumber = true : isGenerateNumber = false;
+	}
+
+	for (var i = 0; i < 4; i++) {
+	    for( var j = 0 ; j < 4 ; j ++ ){
+	    	if (board[i][j] == 0) {
+	    		isGenerateNumber = true;
+	    		break;
+	    	}
+	    }		
+	}	
+
+	return isGenerateNumber ? true : false;
+}
